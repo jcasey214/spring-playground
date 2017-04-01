4cc6a0dfc0607bc6bf1b72fdc2baec263f8ff59d
Jonathan Casey <jon_casey@me.com>
2017-04-01 13:56:16 -0600

    add unit tests for mathService because I just saw that req

---
+++ src/test/java/com/example/service/MathServiceTest.java
@@ -0,0 +1,43 @@
+package com.example.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class MathServiceTest {
+
+    MathService mathService = new MathService();
+
+    @Test
+    public void testCalculateHandlesAddition() throws Exception {
+        String result = mathService.calculate("add", 5, 9);
+        assertEquals("5 + 9 = 14", result);
+    }
+
+    @Test
+    public void testCalculateHandlesSubtraction() throws Exception {
+        String result = mathService.calculate("subtract", 5, 9);
+        assertEquals("5 - 9 = -4", result);
+    }
+
+    @Test
+    public void testCalculateHandlesMultiplication() throws Exception {
+        String result = mathService.calculate("multiply", 5, 9);
+        assertEquals("5 * 9 = 45", result);
+    }
+
+    @Test
+    public void testCalculateHandlesDivision() throws Exception {
+        String result = mathService.calculate("divide", 9, 3);
+        assertEquals("9 / 3 = 3", result);
+    }
+    
+    @Test
+    public void testSumValues() throws Exception {
+        String result = mathService.sumValues(new Integer[]{1,2,3,4,5});
+        assertEquals("1 + 2 + 3 + 4 + 5 = 15", result);
+    }
+}
