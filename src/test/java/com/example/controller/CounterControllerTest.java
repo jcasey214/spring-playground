01e0a0983611686fd40bdedfa3fffbc163ee5b9a
Jonathan Casey <jon_casey@me.com>
2017-05-12 12:04:03 -0600

    security exercise 1

--- src/test/java/com/example/controller/CounterControllerTest.java
+++ src/test/java/com/example/controller/CounterControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -18,6 +19,7 @@ import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc(secure=false)
 public class CounterControllerTest {
 
     @Autowired
--- src/test/java/com/example/controller/HelloControllerTest.java
+++ src/test/java/com/example/controller/HelloControllerTest.java
@@ -4,6 +4,7 @@ import com.example.controller.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HelloController.class)
+@AutoConfigureMockMvc(secure=false)
 public class HelloControllerTest {
 
     @Autowired
